50478a4418511270a6fac137f5fc6f2d427fa26b
GloamingCat <nightlywhiskers@gmail.com>
2024-04-19 17:59:22 -0300

    LPrefs

---
+++ src/lui/base/LPrefs.java
@@ -0,0 +1,11 @@
+package lui.base;
+
+public class LPrefs {
+
+    public static int LABELWIDTH = 60;
+    public static int WIDGETHEIGHT = 25;
+
+    public static int GRIDSPACING = 4;
+    public static int FRAMEMARGIN = 6;
+
+}
